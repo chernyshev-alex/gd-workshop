17d667be5b0ece2dbb5074595979ed9083f02ee2
abelash <oleksiy.byelash@kohls.com>
2020-05-08 01:38:10 +0300

    Predict.py Branch ABE_PROPHET_IMPROVES

--- prophet/apps/predict.py
+++ prophet/apps/predict.py
@@ -1,9 +1,12 @@
 import sys
 import pandas as pd
 from fbprophet import Prophet
+from os.path import abspath
+import matplotlib.pyplot as plt
 
 DATA_DIR = '/data/'
-INPUT_CSV = DATA_DIR + 'aapl.csv'
+#DATA_DIR = '../../data/'
+INPUT_CSV = abspath(DATA_DIR + 'aapl.csv')
 
 if (len(sys.argv) < 2):
     print("Usage : {} days".format(sys.argv[0]))
@@ -16,13 +19,20 @@ csv = pd.read_csv(INPUT_CSV)
 
 df = csv[['Date', 'Close']]
 df.rename(columns={'Date' : 'ds', 'Close' : 'y'}, inplace=True)
+cap = 300
+floor =  10
+df['cap']=cap
+df['floor']=floor
 
 print('train model ..')
 m = Prophet(weekly_seasonality=True, daily_seasonality=True)
+m = Prophet(growth='linear',changepoint_prior_scale=0.05, weekly_seasonality=True, daily_seasonality=True)
 m.fit(df)
 
 print("predict for {} days".format(predict_days))
 future = m.make_future_dataframe(periods=predict_days)
+future['cap']=cap
+future['floor']=floor
 forecast = m.predict(future)
 
 fig1 = m.plot(forecast)
@@ -32,3 +42,4 @@ fig1.savefig(DATA_DIR + 'forecast.png')
 fig2.savefig(DATA_DIR + 'components.png')
 
 print('saved charts to ..' + DATA_DIR)
+#plt.show()
